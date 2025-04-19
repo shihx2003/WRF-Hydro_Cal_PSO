3bceb4c10ab1f26e196a54a86e5928882467230b
shihx2003 <shihx2003@outlook.com>
2025-04-19 16:13:07 +0800

    add : obj_fun

---
+++ core/obj_fun.py
@@ -0,0 +1,62 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   obj_fun.py
+@Create  :   2025-04-19 13:51:11
+@Author  :   shihx2003
+@Version :   1.0
+@Contact :   shihx2003@outlook.com
+'''
+
+# here put the import lib
+import os
+import numpy as np
+import pandas as pd
+
+def Bias(obs, sim):
+    bias_values = {}
+    for colums in sim.columns[1:]:
+        bias_value = np.sum(sim[colums] - obs['obs']) / len(obs['obs'])
+        bias_values[colums] = bias_value
+
+    return bias_values
+
+def PBias(obs, sim):
+    pbias_values = {}
+    for colums in sim.columns[1:]:
+        pbias_value = 100 * (np.sum(sim[colums] - obs['obs']) / np.sum(obs['obs']))
+        pbias_values[colums] = pbias_value
+
+    return pbias_values
+
+def RMSE(obs, sim):
+    rmse_values = {}
+    for colums in sim.columns[1:]:
+        rmse_value = np.sqrt(np.sum((obs['obs'] - sim[colums])**2) / len(obs['obs']))
+        rmse_values[colums] = rmse_value
+    return rmse_values
+
+def CC(obs, sim):
+    cc_values = {}
+    for colums in sim.columns[1:]:
+        cc_value = np.corrcoef(obs['obs'], sim[colums])[0, 1]
+        cc_values[colums] = cc_value
+    return cc_values
+
+def NSE(obs, sim):
+    nse_values = {}
+    for colums in sim.columns[1:]:
+        nse_value = 1 - (np.sum((obs['obs'] - sim[colums])**2) / np.sum((obs['obs'] - np.mean(obs['obs']))**2))
+        nse_values[colums] = nse_value
+    return nse_values
+
+def KGE(obs, sim):
+    kge_values = {}
+    for colums in sim.columns[1:]:
+
+        r = np.corrcoef(obs['obs'], sim[colums])[0, 1]
+        alpha = np.std(sim[colums]) / np.std(obs['obs'])
+        beta = np.mean(sim[colums]) / np.mean(obs['obs'])
+        
+        kge_value = 1 - np.sqrt((r-1)**2 + (alpha-1)**2 + (beta-1)**2)
+        kge_values[colums] = kge_value
+    return kge_values
