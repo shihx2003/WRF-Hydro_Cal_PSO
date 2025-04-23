638a94155e501bbc511cceb704296fc30f8e2b1a
shihx2003 <shihx2003@outlook.com>
2025-04-23 19:45:29 +0800

    __init__.py

---
+++ core/__init__.py
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   __init__.py
+@Create  :   2025-04-23 19:38:15
+@Author  :   shihx2003
+@Version :   1.0
+@Contact :   shihx2003@outlook.com
+'''
+
+# here put the import lib
+
---
+++ util/__init__.py
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   __init__.py
+@Create  :   2025-04-23 19:38:28
+@Author  :   shihx2003
+@Version :   1.0
+@Contact :   shihx2003@outlook.com
+'''
+
+# here put the import lib
