07a34100d208bf283dcfc61aa5057857649693a2
shihx2003 <shihx2003@outlook.com>
2025-05-16 15:29:22 +0800

    merge : creat jobs

--- util/jobs.py
+++ util/jobs.py
@@ -15,8 +15,35 @@ import pandas as pd
 from util.read import read_params_info
 
 def jobs2yaml(paramsnames, param_values, eventname, **kwargs):
+    """
+    Generate a YAML file containing job information based on parameter values.
+    'job_id' =  f'{jobname}_{i+1}'
+    
+    Parameters
+    ----------
+    paramsnames : list
+        List of parameter names.
+    param_values : np.ndarray
+        Array of parameter values for each job.
+    eventname : str
+        Event name for the job.
+    kwargs : dict, optional
+        Additional keyword arguments.
+        - params_path (str): Path to the YAML file containing parameter information, default is './params/run_params.yaml'.
+        - jobname (str): Name of the job, default is 'Run'.
+        - fixed_parmas (dict): Fixed parameters for the job, default is None.
+        - yaml_file (str): Name of the output YAML file, default is '{jobname}_{eventname}.yaml'.
+    
+    Returns
+    ----------
+    jobs : dict
+        Dictionary containing job information.
+    """
     params_path = kwargs.get('params_path', './params/run_params.yaml')
     jobname = kwargs.get('jobname', 'Run')
+    fixed_parmas = kwargs.get('fixed_parmas', None)
+    periods = kwargs.get('periods', None)
+
     basin, no = eventname.split('_')[0], eventname.split('_')[1]
     yaml_file = kwargs.get('yaml_file', f'{jobname}_{eventname}.yaml')
     params_info = read_params_info(params_path, paramsnames)
@@ -24,12 +51,21 @@ def jobs2yaml(paramsnames, param_values, eventname, **kwargs):
     jobs = {}
     for i in range(len(param_values)):
         value = param_values[i]
-        set_params = {}
+        if periods is not None:
+            period = periods[i]
+        else:
+            period = 'Not_Supported'
+        
+        if fixed_parmas is not None:
+            set_params = fixed_parmas.copy()
+        else:
+            set_params = {}
+
         for j, param in enumerate(params_info):
             set_params[param] = float(value[j])
         job_info = {
                 'job_id': f'{jobname}_{i+1}',
-                'period': 'Not_Supported',
+                'period': period,
                 'event_no': f'{eventname}',
                 'basin' : f'{basin}',
                 'set_params': set_params
@@ -44,13 +80,38 @@ def jobs2yaml(paramsnames, param_values, eventname, **kwargs):
 
 
 def jobs2xlsx(yaml_path, xlsx_path=None, **kwargs):
+    """
+    Convert a YAML file containing job information into an Excel file.
+
+    Parameters
+    ----------
+    yaml_path : str
+        Path to the YAML file containing job information.
+    xlsx_path : str, optional
+        Path to save the Excel file. If None, the Excel file will not be saved.
+    kwargs : dict, optional
+    
+
+    Returns
+    ----------
+    
+
+    """
+    return_info = kwargs.get('return_info', False)
     with open(yaml_path, 'r', encoding='utf-8') as f:
         loaded_jobs = yaml.load(f, Loader=yaml.FullLoader)
 
     rows = []
     for sen_id, sen_data in loaded_jobs.items():
         set_params = sen_data['set_params']
-        row = {'job_id': sen_id}
+        if return_info:
+            row =  {'job_id': sen_id,
+                    'event_no': sen_data['event_no'],
+                    'basin': sen_data['basin'],
+                    'period': sen_data['period'],
+                    }
+        else:
+            row = {'job_id': sen_id}
         row.update(set_params)
         rows.append(row)
     params_values = pd.DataFrame(rows)
@@ -58,3 +119,4 @@ def jobs2xlsx(yaml_path, xlsx_path=None, **kwargs):
         params_values.to_excel(xlsx_path, index=False)
 
     return params_values
+
