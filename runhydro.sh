467a551954a3fa05d2e032461b577f9813685c11
shihx2003 <shihx2003@outlook.com>
2025-05-16 15:38:43 +0800

    add : run pso

---
+++ runhydro.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp -r /public/home/Shihuaixuan/Run/Haihe_bak/Haihe_run_source ./run_source
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate sen
+echo "The environment is activated and script is running."
+nohup python3 run_pso.py > ./run_pso.log 2>&1 &
+echo "Python script is running in the background. Check the log file for output."
+ps aux | grep run_pso.py
---
+++ util/correct.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   correct.py
+@Create  :   2025-04-29 20:03:05
+@Author  :   shihx2003
+@Version :   1.0
+@Contact :   shihx2003@outlook.com
+'''
+
+# here put the import lib
+import os
+import pandas as pd
+from util.wrfhydrofrxst import ConvertTimeZone
+def correct_sim(sim, eventname):
+    sim = sim.copy()
+    return sim
