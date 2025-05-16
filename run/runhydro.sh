2c15aec482eaef48dcb8bec7e7a2c37919a44beb
shihx2003 <shihx2003@outlook.com>
2025-05-16 16:12:32 +0800

    del

--- run/runhydro.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cp -r /public/home/Shihuaixuan/Run/Haihe_bak/Haihe_run_source ./run_source
-source ~/anaconda3/etc/profile.d/conda.sh
-conda activate sen
-echo "The environment is activated and script is running."
-nohup python3 run_Flood_Ana.py > ./run_Flood_Ana.log 2>&1 &
-echo "Python script is running in the background. Check the log file for output."
-ps aux | grep run_Flood_Ana.py
